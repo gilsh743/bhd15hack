86851f14d3ab65a9182daed4f4baa2e1d6f81484
Jessica <tovtov989@gmail.com>
2019-06-16 11:22:01 -0500

    adding good textbox

--- src/Text/OriginalTextBox/OriginalTextBox.component.js
+++ src/Text/OriginalTextBox/OriginalTextBox.component.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import CKEditor from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-// import { TextInput } from 'react-native';
+import TextArea from 'react-textarea-autosize'
+
 export default class OriginalTextBox extends React.Component {
     constructor(props) {
         super(props);
@@ -11,7 +12,8 @@ export default class OriginalTextBox extends React.Component {
     render() {
         return (
             <div>
-                <input className="textBox" placeholder={this.props.placeholder} disabled={this.state.isDisabled}/>
+                {/* <input className="textBox" placeholder={this.props.placeholder} disabled={this.state.isDisabled}/> */}
+                <TextArea onKeyDownCapture={e => console.log("event", e)} autoFocus className="textBox" placeholder={this.props.placeholder} disabled={this.state.isDisabled}/>>
                 <button onClick={() => this.setState({isDisabled: true})}>Translate</button>
                 <button onClick={() => this.props.onSelected(window.getSelection().toString())}>Selection</button>
 

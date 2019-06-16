79946fe04cc158570cfd06b199e9f80599dbbed0
Jessica <tovtov989@gmail.com>
2019-06-16 10:48:12 -0500

    Changing order

--- src/Text/Text.js
+++ src/Text/Text.js
@@ -1,23 +1,32 @@
 
 import React from 'react';
 import TranslationInput from './translation-input';
+import OriginalTextBox from './OriginalTextBox/OriginalTextBox.component'
 
 
 class Text extends React.Component {
     constructor(props){
         super(props);
-        this.state = {transText: " " };
+        this.state = {transText: " " , orgText: ""};
 
         this.onTransTextChange = this.onTransTextChange.bind(this);
+        this.onSelected = this.onSelected.bind(this);
     };
 
     onTransTextChange(event){
         this.setState({transText: event.target.value});
     };
 
+    onSelected() {
+        this.setState({orgText: window.getSelection().toString()})
+    }
+
     render() {
         return (
         <div>
+            <OriginalTextBox onSelected={this.onSelected} />
+            {this.state.orgText}
+
             <TranslationInput description={"write your translation here: "} onchange={this.onTransTextChange} />
             {this.state.transText}
         </div>

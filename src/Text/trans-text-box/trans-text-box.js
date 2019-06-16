947feeb308546cb86e20d04d6fad18408873fa04
Gil Shafat <gish43@gmail.com>
2019-06-17 02:25:18 +0300

    Format.

--- src/Text/trans-text-box/trans-text-box.js
+++ src/Text/trans-text-box/trans-text-box.js
@@ -13,23 +13,23 @@ class TransTextBox extends React.Component {
         return (
             <div>
                 <CKEditor
-          editor={ ClassicEditor }
-          data="<p>Hello from CKEditor 5!</p>"
-          onInit={ editor => {
-              // You can store the "editor" and use when it is needed.
-              console.log( 'Editor is ready to use!', editor );
-          } }
-          onChange={ ( event, editor ) => {
-              const data = editor.getData();
-              console.log( { event, editor, data } );
-          } }
-          onBlur={ editor => {
-              console.log( 'Blur.', editor );
-          } }
-          onFocus={ editor => {
-              console.log( 'Focus.', editor );
-          } }
-      />
+                    editor={ClassicEditor}
+                    data="<p>Hello from CKEditor 5!</p>"
+                    onInit={editor => {
+                        // You can store the "editor" and use when it is needed.
+                        console.log('Editor is ready to use!', editor);
+                    }}
+                    onChange={(event, editor) => {
+                        const data = editor.getData();
+                        console.log({ event, editor, data });
+                    }}
+                    onBlur={editor => {
+                        console.log('Blur.', editor);
+                    }}
+                    onFocus={editor => {
+                        console.log('Focus.', editor);
+                    }}
+                />
             </div>
         )
     }
